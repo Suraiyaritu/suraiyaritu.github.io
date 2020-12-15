56bf2fd5b6cc61ed0a1b7b7b3cd4ca184c51f54e
Suraiyaritu <ritu90130@gmail.com>
2020-12-15 15:18:10 +0600

    Comment section

---
+++ Comment section/app.js
@@ -0,0 +1,74 @@
+const inputComment = document.querySelector('.input-comment');
+const pressBtn = document.querySelector('.press-btn');
+const textLists = document.querySelector('.text-lists');
+
+
+pressBtn.addEventListener('click', textListsCreate);
+window.addEventListener('keypress', keyShow);
+window.addEventListener('DOMContentLoaded', getText);
+
+
+function keyShow(e) {
+    if (e) {
+        pressBtn.classList.add('press-btn-remove');
+    } else {
+        pressBtn.classList.remove('press-btn-remove');
+    }
+}
+
+function textListsCreate(e) {
+    e.preventDefault();
+    const item = document.createElement('li');
+    item.classList.add('text-item');
+
+    if (inputComment.value == '') {
+        item.style.display = 'none';
+        pressBtn.classList.remove('press-btn-remove');
+    } else {
+        item.innerText = inputComment.value;
+        pressBtn.classList.add('press-btn-remove');
+    }
+    saveToLocal(inputComment.value)
+    textLists.appendChild(item);
+
+    inputComment.value = '';
+}
+
+
+function saveToLocal(text){
+  let textItems;
+  if(localStorage.getItem('textItems')===null){
+     textItems =[];
+  }else{
+      textItems =JSON.parse(localStorage.getItem('textItems'))
+}
+textItems.push(text);
+localStorage.setItem('textItems', JSON.stringify(textItems))
+}
+// localStorage.clear()
+
+function getText(){
+    let textItems;
+  if(localStorage.getItem('textItems')===null){
+     textItems =[];
+  }else{
+      textItems =JSON.parse(localStorage.getItem('textItems'))
+}
+
+textItems.forEach(function(titems){
+    const item = document.createElement('li');
+    item.classList.add('text-item');
+   
+    if(item.innerText = titems){
+        
+        pressBtn.classList.add('press-btn-remove');
+    }else{
+        item.style.display= 'none'
+         pressBtn.classList.remove('press-btn-remove');
+        
+    }
+    textLists.appendChild(item);
+   
+
+})
+}
